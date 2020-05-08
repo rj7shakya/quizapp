3323449aa33ebe1750491bc46a410dad0013a1ab
rj7shakya <shakyarajad1@gmail.com>
2020-05-08 17:51:06 +0545

    navbar added

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,33 +3,6 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    // <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-    //   <i className="fa fa-question m-2" aria-hidden="true"></i>
-    //   <a className="navbar-brand" href="#">
-    //     Quiz app
-    //   </a>
-    //   <button
-    //     className="navbar-toggler"
-    //     type="button"
-    //     data-toggle="collapse"
-    //     data-target="#navbarNavAltMarkup"
-    //     aria-controls="navbarNavAltMarkup"
-    //     aria-expanded="false"
-    //     aria-label="Toggle navigation"
-    //   >
-    //     <span className="navbar-toggler-icon"></span>
-    //   </button>
-    //   <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    //     <div className="navbar-nav">
-    //       <Link to="/" className="m-2">
-    //         Quiz
-    //       </Link>
-    //       <Link to="/setting" className="m-2">
-    //         Setting
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </nav>
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <a href="#" className="navbar-brand">
         Quiz app

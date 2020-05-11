3fce547e0c41c409d30e8a0561e50d94c97e7cdf
rj7shakya <shakyarajad1@gmail.com>
2020-05-11 19:14:47 +0545

    sql node

--- routes/question.js
+++ routes/question.js
@@ -9,22 +9,30 @@ router.get("/", (req, res) => {
     "SELECT question.question_id,question.question,choice.choice,choice.is_correct FROM question INNER JOIN choice ON question.question_id=choice.question_id order by question.question_id",
     (err, rows, field) => {
       if (err) return res.json({ error: err });
-      let datas = "";
-      let data = "";
+      // let datas = "";
+      // let data = "";
       var choiceArr = [];
 
       rows.map((row) => {
-        datas = datas + "," + row.choice;
+        // datas = datas + "," + row.choice;
+
+        // if (questions[row.question_id] !== undefined) {
+        //   data = questions[row.question_id].choice;
+        // }
+        if (row.choice !== undefined) {
+          choiceArr[row.question_id - 1] += "," + row.choice;
+        }
+        let indx = row.question_id;
         questions[row.question_id] = {
           ...row,
-          choices: { datas },
+          // choices: [...choiceArr].slice(),
+          choices: choiceArr.filter(function (value, index) {
+            console.log(row.question_id);
+            return index === row.question_id;
+          }),
         };
-        if (questions[row.question_id] !== undefined) {
-          data = questions[row.question_id].choice;
-        }
-        choiceArr[row.question_id] = data + row.choice;
-        console.log("data", datas, row.question_id);
-        console.log(choiceArr);
+        console.log("data", row.question_id);
+        // console.log(choiceArr[3]);
       });
       console.log("next");
       res.json({ questions });
